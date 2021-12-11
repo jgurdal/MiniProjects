1476808033f1d682752ba55195a350e5bbae491e
Jonathan <jgurdal@mail.sfsu.edu>
2021-12-11 10:50:02 -0800

    Added update loop
    Created an update loop that refreshes based on user refresh

--- DinoRunner/script.js
+++ DinoRunner/script.js
@@ -4,4 +4,34 @@ const WORLD_HEIGHT = 30
 const worldELem = document.querySelector('[data-world]')
 
 setPixelToWorldScale()
-window.addEventListener("resize", setPixelToWorldScale)
+window.addEventListener("resize", setPixelToWorldScale) 
+
+let lastTime
+function update(time) {
+    // Make the initial timing more consistent
+    if(lastTime === null) {
+        lastTime = time 
+        window.requestAnimationFrame(update)
+        return
+    }
+    const delta = lastTime - time
+
+    lastTime = time
+    // Loop 
+    window.requestAnimationFrame(update)
+}
+// Only calls the update fn the next time we update our screen
+window.requestAnimationFrame(update)
+
+function setPixelToWorldScale() {
+    let worldToPixelScale
+    if(window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT)
+    {
+        worldToPixelScale = window.innerWidth / WORLD_WIDTH
+    } else {
+        worldToPixelScale = window.innerHeight / WORLD_HEIGHT
+    }
+
+    worldELem.style.width = `${WORLD_WIDTH * worldToPixelScale}px`
+    worldELem.style.height = `${WORLD_HEIGHT * worldToPixelScale}px`
+}
