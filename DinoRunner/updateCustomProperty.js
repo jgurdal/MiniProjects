b8c14df138549e890d6a752628581952275c69c9
Jonathan <jgurdal@mail.sfsu.edu>
2021-12-19 22:25:13 -0800

    Added cactus and dinosaur movement
    created and updated cactus

--- DinoRunner/updateCustomProperty.js
+++ DinoRunner/updateCustomProperty.js
@@ -1,15 +1,14 @@
-//helper file
 export function getCustomProperty(elem, prop) {
-    // getComputedStyle(elem) lets us get CSS variables
+        // getComputedStyle(elem) lets us get CSS variables
     // getPropertyValue(prop) returns a string
     return parseFloat(getComputedStyle(elem).getPropertyValue(prop)) || 0
-}
-
-export function setCustomProperty(elem, prop, value) {
-    elem.style.setProperty(prop,value)
-}
-
-export function incrementCustomProperty() {
+  }
+  
+  export function setCustomProperty(elem, prop, value) {
+    elem.style.setProperty(prop, value)
+  }
+  
+  export function incrementCustomProperty(elem, prop, inc) {
     //get the value add the increment and set the value
-    setCustomProperty(elem,prop, getCustomProperty(elem,prop) + inc)
-}
+    setCustomProperty(elem, prop, getCustomProperty(elem, prop) + inc)
+  }
